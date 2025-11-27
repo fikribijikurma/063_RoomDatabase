7f4cd685a667d86bd4363f7090f3bc726d959b7f
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 13:49:49 +0700

    membuat fungsi ekstensi untuk mengonversi data Siswa

--- app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/datasiswa/viewmodel/EntryViewModel.kt
@@ -2,7 +2,10 @@ package com.example.datasiswa.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.example.datasiswa.repositori.RepositoriSiswa
+import com.example.datasiswa.room.Siswa
 
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
@@ -38,6 +41,17 @@ data class DetailSiswa(
     val telpon: String = "",
 )
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
