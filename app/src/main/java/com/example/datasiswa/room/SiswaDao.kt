dae3519eac3aa364e59fb4cc041253cdb0db88e5
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 13:01:03 +0700

    membuat interface SiswaDao

---
+++ app/src/main/java/com/example/datasiswa/room/SiswaDao.kt
@@ -0,0 +1,18 @@
+package com.example.datasiswa.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+
+    @Query("SELECT * from tabel_siswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+}
