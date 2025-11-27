0141d2133ddfa1bde4804d8fb41c4297d98904fa
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 14:17:42 +0700

    membuat file PetaNavigasi dan composable SiswaApp dan HostNavigasi

---
+++ app/src/main/java/com/example/datasiswa/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.datasiswa.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
