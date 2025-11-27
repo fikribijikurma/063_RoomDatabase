83d5d873f1b82082f2c167bb4b33a115e2bc792c
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 14:34:15 +0700

    membuat BodyHome dan menampilkan data siswa

--- app/src/main/java/com/example/datasiswa/view/HalamanHome.kt
+++ app/src/main/java/com/example/datasiswa/view/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.datasiswa.view
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
@@ -51,3 +53,15 @@ fun HomeScreen(
             }
         },
     ){
+            innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+
