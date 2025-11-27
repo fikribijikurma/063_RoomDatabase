0633891f81951317a565c6a1c61c557f948e30fb
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 14:04:48 +0700

    membuat fungsi ekstensi untuk kelas CreationExtras

--- app/src/main/java/com/example/datasiswa/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/datasiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.datasiswa.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.sqlite.db.SupportSQLiteOpenHelper
+import com.example.datasiswa.repositori.AplikasiSiswa
+import com.example.datasiswa.viewmodel.EntryViewModel
+import com.example.datasiswa.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -14,3 +20,9 @@ object PenyediaViewModel {
         }
     }
 }
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory
+        .APPLICATION_KEY] as AplikasiSiswa)
+
+
