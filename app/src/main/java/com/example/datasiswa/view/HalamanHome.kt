48213f39091e868168f4e27223463b0597b7a3b0
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-27 14:36:06 +0700

    membuat ListSiswa dan DataSiswa

--- app/src/main/java/com/example/datasiswa/view/HalamanHome.kt
+++ app/src/main/java/com/example/datasiswa/view/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.datasiswa.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -90,5 +92,20 @@ fun BodyHome(
         }
     }
 }
+@Composable
+fun ListSiswa(
+    itemSiswa : List<Siswa>,
+    modifier: Modifier=Modifier
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+                person ->  DataSiswa(
+            siswa = person,
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_small)))
+        }
+    }
+}
+
 
 
