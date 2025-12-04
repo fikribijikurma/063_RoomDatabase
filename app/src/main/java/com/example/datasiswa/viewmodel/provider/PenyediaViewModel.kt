2a7f828b622f2d311835d410e462040fc6a3acd4
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-04 12:02:53 +0700

    membuat DetailViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/datasiswa/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/datasiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -1,22 +1,28 @@
 package com.example.datasiswa.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.example.datasiswa.repositori.AplikasiSiswa
+import com.example.datasiswa.viewmodel.DetailViewModel
 import com.example.datasiswa.viewmodel.EntryViewModel
 import com.example.datasiswa.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+            HomeViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
         }
 
         initializer {
-            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+            EntryViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().containerApp.repositoriSiswa)
         }
     }
 }
