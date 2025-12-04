3d2db9d960dad087e66a0cce63aec4a2bcb17aab
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-04 11:30:08 +0700

    feat: Menambahkan fungsi update, delete, dan getSiswa pada SiswaDao

--- app/src/main/java/com/example/datasiswa/room/SiswaDao.kt
+++ app/src/main/java/com/example/datasiswa/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.datasiswa.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,5 +16,14 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tabel_siswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 
 }
