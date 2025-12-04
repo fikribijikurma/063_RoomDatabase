7bc7be1ea6e9242b007ca70863b5e5e24de3f191
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-12-04 13:54:28 +0700

    mengubah package pada DestinasiDetailSiswa

--- app/src/main/java/com/example/datasiswa/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/datasiswa/view/route/DestinasiDetailSiswa.kt
@@ -1,11 +1,13 @@
-package com.example.myroomsiswa.view.route
+package com.example.datasiswa.view.route
 
 import com.example.datasiswa.R
-import com.example.datasiswa.view.route.DestinasiNavigasi
+
 
 object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
+
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
+
     val routeWithArgs = "$route/{$itemIdArg}"
 }
